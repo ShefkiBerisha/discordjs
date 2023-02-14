294f28da35f693c3dca51ba00642ea7154ae7ee2
ShefkiBerisha <94741859+ShefkiBerisha@users.noreply.github.com>
2023-02-14 14:47:23 +0100

    Update ping.js

--- src/Commands/ping.js
+++ src/Commands/ping.js
@@ -5,6 +5,17 @@ module.exports = {
     .setName("ping")
     .setDescription("Get the bots latency!"),
   execute: async (interaction, client) => {
-    return interaction.reply({ content: `Pong \`${client.ws.ping}ms\` 🏓` });
+    let days = Math.floor(client.uptime / 86400000)
+    let hours= Math.floor(client.uptime / 3600000) % 24
+    let minutes = Math.floor(client.uptime / 60000) % 60
+    let seconds = Math.floor(client.uptime / 1000) % 60
+    
+    const embed = new EmbedBuilder()
+        .setTitle(`🤖 **Bot Latency:** \n \`${Date.now() - interaction.createdTimestamp}ms\``)
+        .addFields({name:`📱 Websocket Ping:`, value:`\`${client.ws.ping}ms\``})
+        .addFields({name:`✅ Uptime:`, value:`\`${days}\` days, \`${hours}\` hours, \`${minutes}\` minutes, \`${seconds}\` seconds.`})
+        .setColor("Green")
+    await interaction.reply({embeds: [embed], ephemeral=true})
+
   },
 };
